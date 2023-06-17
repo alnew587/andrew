1e92816cbe5362f1ede8c1c206a73d4d8129d277
alexbnewman <alex02new@outlook.com>
2023-06-17 18:18:21 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import opal from './assets/red opal.jpg';
+import cowboy from './assets/IMG_3071.jpg';
+import us from './assets/IMG_1962.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="Pad"></div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="col">
+          <img src={cowboy} className="pic" alt="cowboy" />
+        </div>
+
+        <div className="col">
+        <img src={opal} className="App-logo" alt="opal" />
+        <div className="textBack">
+          <p>
+            happy birthday!
+          </p>
+        </div>
+        </div>
+
+        <div className="col">
+          <img src={us} className="pic" alt="us two" />
+        </div>
+
       </header>
     </div>
   );
